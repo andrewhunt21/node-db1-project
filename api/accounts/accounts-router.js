c7eb38830b470a3f387464498fa42694f8530212
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-18 16:57:07 -0800

    passes all tests

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -30,7 +30,10 @@ router.post(
   async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    const newAccount = await Account.create(req.body)
+    const newAccount = await Account.create({ 
+      name:req.body.name.trim(), 
+      budget: req.body.budget, 
+    })
     res.status(201).json(newAccount)
   } catch (err) {
     next(err)
@@ -43,10 +46,9 @@ router.put(
   md.checkAccountPayload,
   async (req, res, next) => {
     const updated = await Account.updateById(req.params.id, req.body)
-    res.json(updated)
   // DO YOUR MAGIC
     try {
-      res.json('update account')
+      res.json(updated)
     } catch (err) {
       next(err)
     }
